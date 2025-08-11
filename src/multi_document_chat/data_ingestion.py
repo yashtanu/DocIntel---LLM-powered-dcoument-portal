e74cc5ee92c036676b7947c7e889b8d8eb118d5d
firstcryUAE <yash.agrawal@firstcry.ae>
2025-08-11 11:54:54 +0400

    multidoc chat initialised

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,65 @@
+
+import uuid
+from pathlib import Path
+import sys
+from datetime import datetime, timezone
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from utils.model_loader import ModelLoader
+
+
+class DocumentIngestor:
+    SUPPORTED_FILE_TYPES = {'.pdf','.docx','.txt','.md'}
+    def __init__(self,temp_dir:str = "data/multi_doc_chat",faiss_dir: str = "faiss_index",session_id :str | None = None):
+        try:
+            self.log = CustomLogger().get_logger(__name__)
+
+            # base directory initiate
+            self.temp_dir=Path(temp_dir)
+            self.faiss_dir  =Path(faiss_dir)
+            self.temp_dir.mkdir(parents=True,exist_ok=True)
+            self.faiss_dir.mkdir(parents=True,exist_ok=True)
+            
+            # sessionized paths 
+            self.session_id = session_id or f"session_{datetime.now(timezone.utc).strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
+            self.session_temp_dir = self.temp_dir/self.session_id
+            self.session_faiss_dir = self.faiss_dir/self.session_id
+            self.session_temp_dir.mkdir(parents=True,exist_ok=True)
+            self.session_faiss_dir.mkdir(parents=True,exist_ok=True)
+
+            self.model_loader = ModelLoader()
+            self.log.info(
+                "DocumentIngestor initialised",
+                temp_base = str(self.temp_dir)
+                faiss_base = str(self.faiss_dir)
+                session_id = self.session_id
+                temp_path = str(self.session_temp_dir)
+                faiss_path = str(self.session_faiss_dir)
+            )
+        except Exception as e:
+            self.log.error("failed to inialise DocumentIngestor",error=str(e))
+            raise DocumentPortalException("Inialization error in DocumentIngestor",sys)
+        
+
+    def ingest_files(self):
+        try:
+            pass
+        except Exception as e:
+            self.log.error("failed to inialise DocumentIngestor",error=str(e))
+            raise DocumentPortalException("Inialization error in DocumentIngestor",sys)
+        
+
+    def _create_retriever(self,documents):
+        try:
+            pass
+        except Exception as e:
+            self.log.error("failed to inialise DocumentIngestor",error=str(e))
+            raise DocumentPortalException("Inialization error in DocumentIngestor",sys)
+
+
+
+
+
