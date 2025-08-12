5d5205475a63991b44b312d04cdd31ec82978c90
firstcryUAE <yash.agrawal@firstcry.ae>
2025-08-12 15:29:42 +0400

    folder refactoring is done

---
+++ versions.py
@@ -0,0 +1,30 @@
+import importlib.metadata
+packages = [
+    "langchain",
+    "python-dotenv",
+    "ipykernel",
+    "langchain_groq",
+    "langchain_google_genai",
+    "langchain-community",
+    "faiss-cpu",
+    "structlog",
+    "PyMuPDF",
+    "pylint",
+    "langchain-core",
+    "pytest",
+    "streamlit",
+    "fastapi",
+    "uvicorn",
+    "python-multipart",
+    "docx2txt"
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
+
+# # serve static & templates
+# app.mount("/static", StaticFiles(directory="../static"), name="static")
+# templates = Jinja2Templates(directory="../templates")
